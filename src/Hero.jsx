1ab56a5599b5b9225646be765d086d37e8d453b4
Papa-Sean <smsugrue@gmail.com>
2024-03-05 14:11:08 -0500

    added hero

---
+++ src/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <section className="hero" id="home">
+            <div className="hero-banner">
+                <h1>paradizo films</h1>
+                <p>
+                    Play along at home and write an episode of your favorite sitcom just like the BOIZ!
+                </p>
+                <a href="#play" className="btn hero-btn">start writing</a>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
