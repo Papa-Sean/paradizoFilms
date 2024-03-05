cbaf5e7aa29f0135ba7bc28cb6b5d5da505c1c42
Papa-Sean <smsugrue@gmail.com>
2024-03-05 13:45:54 -0500

    initial commit

---
+++ src/data.jsx
@@ -0,0 +1,11 @@
+export const socialLinks = [
+    {id: 1, href: "https://www.instagram.com/paradizofilms/", icon: 'fab fa-instagram'},
+    {id: 2, href: "https://www.facebook.com/joseph.naman.54", icon: 'fab fa-facebook'},
+    {id: 3, href: "https://www.youtube.com/@paradizofilms", icon: 'fab fa-youtube'}
+]
+
+export const pageLinks = [
+    {id: 1, href: "#home", text: "home"},
+    {id: 2, href: "#store", text: "store"},
+    {id: 3, href: "#play", text: "play"}
+]
